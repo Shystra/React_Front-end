26f53769c536d640e70e2da3d8f015ebf1824513
Shystra <jlsp.2019@hotmail.com>
2023-04-26 22:30:01 -0300

    testing "styled components"

--- project/src/components/App/index.jsx
+++ project/src/components/App/index.jsx
@@ -1,9 +1,13 @@
-import './App.css';
+import * as Styled from './styles'
+
+
 
 function Home() {
   return (
     <div className="App">
+      <Styled.Wrapper>
       <h1>Hello</h1>
+      </Styled.Wrapper>
     </div>
   );
 }

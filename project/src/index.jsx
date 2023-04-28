21e6fabd717f623cddaba01645c5c3e172c6ea08
Shystra <jlsp.2019@hotmail.com>
2023-04-28 18:44:52 -0300

    config styles components

--- project/src/index.jsx
+++ project/src/index.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+
+
+import { ThemeProvider } from 'styled-components';
 import ReactDOM from 'react-dom/client';
 import Home from './components/App';
-import { GlobalStyles } from './styles/global-styles';
+import { GlobalStyles } from './components/styles/global-styles';
+import { theme } from './components/styles/theme';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <GlobalStyles />
     <Home />
+    </ThemeProvider>
   </React.StrictMode>
 );
 

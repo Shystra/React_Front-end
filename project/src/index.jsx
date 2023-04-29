816978151c8eb240abd1e651c5c17eb46c9a09f9
Shystra <jlsp.2019@hotmail.com>
2023-04-28 22:01:08 -0300

    configuring and testing the "Storybook"

--- project/src/index.jsx
+++ project/src/index.jsx
@@ -1,19 +1,29 @@
 import React from 'react';
 
 
-import { ThemeProvider } from 'styled-components';
+import styled, { ThemeProvider, css } from 'styled-components';
 import ReactDOM from 'react-dom/client';
 import Home from './components/App';
 import { GlobalStyles } from './components/styles/global-styles';
 import { theme } from './components/styles/theme';
+import { Heading } from './components/Heading';
 
+
+export const Container = styled.div`
+  background: red;
+  
+  ${(props) => css`
+    color: ${props.theme.colors.white};
+  `}
+`;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-    <GlobalStyles />
-    <Home />
+      <Heading />
+      <GlobalStyles />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>
 );

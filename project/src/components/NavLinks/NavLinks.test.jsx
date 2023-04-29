75cf402bd6611ce4b1506c1358a823aa60c531e4
Shystra <jlsp.2019@hotmail.com>
2023-04-29 19:57:40 -0300

    creating tests about links

--- project/src/components/NavLinks/NavLinks.test.jsx
+++ project/src/components/NavLinks/NavLinks.test.jsx
@@ -1,10 +1,21 @@
 import { screen } from '@testing-library/react';
 import { NavLinks } from '.'
 import { renderTheme } from '../styles/render_theme';
+import mock from './mock';
 
 describe('<NavLinks />', () => {
-    it('should render', () => {
-        renderTheme(<NavLinks>Children</NavLinks>);
-        expect(screen.getByRole('heading')).toBeInTheDocument();
+    it('should render links', () => {
+        renderTheme(<NavLinks links={mock} />);
+        expect(screen.getAllByRole('link')).toHaveLength(mock.length);
+    });
+
+    it('should not render links', () => {
+        renderTheme(<NavLinks />);
+        expect(screen.queryAllByAltText(/links/i)).toHaveLength(0);
+    });
+
+    it('should match snapshot', () => {
+        renderTheme(<NavLinks links={mock} />);
+        expect(screen.getAllByRole('link')).toMatchSnapshot();
     });
 });

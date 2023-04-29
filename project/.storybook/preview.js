a7b2b7fe02a90e3a7446ac5dbc488b1f04702034
Shystra <jlsp.2019@hotmail.com>
2023-04-28 23:20:31 -0300

    cheking view "stories.jsx" on Storybook...

--- project/.storybook/preview.js
+++ project/.storybook/preview.js
@@ -6,6 +6,20 @@ import { theme } from '../src/components/styles/theme';
 const preview = {
   parameters: {
     actions: { argTypesRegex: '^on[A-Z].*' },
+    backgrounds: {
+      default: 'light',
+      values: [
+        {
+          name: 'light',
+          value: theme.colors.white,
+        },
+        {
+          name:'dark', 
+          value: theme.colors.primaryColor,
+        }
+      ]
+    },
+
     controls: {
       matchers: {
         color: /(background|color)$/i,
--- project/src/components/Heading/stories.jsx
+++ project/src/components/Heading/stories.jsx
@@ -4,11 +4,38 @@ export default {
   title: 'Heading',
   component: Heading,
   args: {
-    children: 'Não sei',
+    children: 'O texto esta escuro',
+    light: false,
   },
   argTypes: {
     children: { type: 'string' },
+    light: { type: 'boolean' },
+  },
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+    },
   },
 };
 
-export const Template = (args) => <Heading {...args} />;
+export const Light = (args) => <Heading {...args} />;
+export const Dark = (args) => <Heading {...args} light={true} />;
+
+Light.parameters = {
+  backgrounds: {
+    default: 'light',
+  },
+};
+Light.args = {
+  children: 'O texto está claro',
+  light: false,
+}
+
+
+
+
+
+
+
+
+

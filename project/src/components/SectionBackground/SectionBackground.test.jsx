39ff3281ba1259b29b89a425a9125c7c8465fac3
Shystra <jlsp.2019@hotmail.com>
2023-04-29 21:58:24 -0300

    testing background and container

--- project/src/components/SectionBackground/SectionBackground.test.jsx
+++ project/src/components/SectionBackground/SectionBackground.test.jsx
@@ -4,7 +4,18 @@ import { renderTheme } from '../styles/render_theme';
 
 describe('<SectionBackground />', () => {
     it('should render', () => {
-        renderTheme(<SectionBackground>Children</SectionBackground>);
+        const { container } = renderTheme(<SectionBackground background={true}><h1>Children</h1></SectionBackground>);
         expect(screen.getByRole('heading')).toBeInTheDocument();
+        expect(container).toMatchSnapshot();
     });
+
+    it('should render with backgroud light', () => {
+        const { container } = renderTheme(
+            <SectionBackground>
+                <h1>Children</h1>
+            </SectionBackground>,
+        );
+        expect(screen.getByRole('heading')).toBeInTheDocument();
+        expect(container).toMatchSnapshot();
+    })    
 });
--- project/src/components/SectionContainer/SectionContainer.test.jsx
+++ project/src/components/SectionContainer/SectionContainer.test.jsx
@@ -3,8 +3,10 @@ import { SectionContainer } from '.'
 import { renderTheme } from '../styles/render_theme';
 
 describe('<SectionContainer />', () => {
-    it('should render', () => {
-        renderTheme(<SectionContainer>Children</SectionContainer>);
+    it('should render content', () => {
+        const { container } = renderTheme(<SectionContainer><h1>Children</h1></SectionContainer>,);
+        
         expect(screen.getByRole('heading')).toBeInTheDocument();
+        expect(container).toMatchSnapshot();
     });
 });

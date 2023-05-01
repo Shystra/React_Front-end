addd375af6ad31d386c63717b697b3e517520576
Shystra <jlsp.2019@hotmail.com>
2023-05-01 12:33:12 -0300

    creating menu

---
+++ project/src/components/Menu/styles.js
@@ -0,0 +1,14 @@
+import styled, {css} from 'styled-components';
+
+export const Container = styled.div`
+    ${({ theme }) => css`
+        position: fixed;
+        z-index: 5;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+                       
+    
+    `}
+`;

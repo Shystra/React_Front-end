315e94a7216dbad8ec2af362981d802d23b8aced
Shystra <jlsp.2019@hotmail.com>
2023-05-06 10:56:13 -0300

    Configuring GridContent

--- project/src/components/GridContent/index.jsx
+++ project/src/components/GridContent/index.jsx
@@ -1,16 +1,26 @@
 import P from 'prop-types';
+import { Heading } from '../Heading';
+import { SectionBackground } from '../SectionBackground';
+import { TextComponent } from '../TextComponent';
 import * as Styled from './styles';
 
-
-export const GridContent = ({ children }) => {
-    return (
-        <Styled.Container>
-            <h1>{children}</h1>
-        </Styled.Container>
-    );
-    
+export const GridContent = ({ title, html, background = false }) => {
+  return (
+    <SectionBackground background={background}>
+      <Styled.Container>
+        <Heading uppercase colorDark={!background}>
+          {title}
+        </Heading>
+        <Styled.Html>
+          <TextComponent>{html}</TextComponent>
+        </Styled.Html>
+      </Styled.Container>
+    </SectionBackground>
+  );
 };
 
 GridContent.propTypes = {
-    children: P.node.isRequired,
-}
+  title: P.string.isRequired,
+  html: P.string.isRequired,
+  background: P.bool,
+};
--- project/src/components/GridContent/styles.js
+++ project/src/components/GridContent/styles.js
@@ -1,8 +1,19 @@
 import styled, { css } from 'styled-components';
 
 
-export const GridContent = styled.div`
+export const Container = styled.div`
     ${({ theme }) => css`
+        text-align: center;
+        max-width: 58rem;
+        margin: 0 auto;
+
+    
+    `}
+`;
+
+export const Html = styled.div`
+    ${({ theme }) => css`
+        margin: ${theme.spacings.xhuge} 0;
     
     
     `}

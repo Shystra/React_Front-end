ce6d9fbbe1de52afc95f85264ea889a250db9416
Shystra <jlsp.2019@hotmail.com>
2023-05-08 23:04:52 -0300

    just position

--- project/src/components/GridImage/index.jsx
+++ project/src/components/GridImage/index.jsx
@@ -9,19 +9,16 @@ export const GridImage = ({ title, description, grid, background = false }) => {
     return (
         <SectionBackground background={background}>
             <Styled.Container>
-                <Heading size="huge" uppercase colorDark={!background} as="h2">
-                    {title}
-                </Heading>
-                <TextComponent>{description}</TextComponent>
-                <Styled.Grid>
-                    {grid.map((el) => (
-                        <Styled.GridElement key={`${el.srcImg}${el.altText}`}>
-                            <Styled.Image src={el.srcImg} alt={el.altText} />
-                        </Styled.GridElement>
-                    ))}
-                </Styled.Grid>
-            </Styled.Container>
-        </SectionBackground>
+                <Heading size="huge" uppercase colorDark={!background} as="h2">{title} </Heading>
+                
+                    <TextComponent>{description}</TextComponent>
+                        <Styled.Grid>{grid.map((el) => (
+                            <Styled.GridElement key={`${el.srcImg}${el.altText}`}>
+                                <Styled.Image src={el.srcImg} alt={el.altText} />
+                            </Styled.GridElement> ))}
+                        </Styled.Grid>
+                 </Styled.Container>
+            </SectionBackground>
     );
 };
 

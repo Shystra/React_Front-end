401de9809c5b7f8fa635da1198d717229fb54caf
Shystra <jlsp.2019@hotmail.com>
2023-05-10 19:48:09 -0300

    making a new footer

---
+++ project/src/components/Footer/stories.jsx
@@ -0,0 +1,17 @@
+import { Footer } from '.';
+
+export default {
+    title: 'Footer',
+    component: Footer,
+    args: {
+        html: `<p><a href="https://github.com/Shystra">Made by Lucas</a></p>`,
+    },
+};
+
+export const Template = (args) => {
+    return (
+        <div>
+            <Footer {...args} />
+        </div>
+    );
+};

288f39679f5ca4db90bec64137bf1510534caa80
Shystra <jlsp.2019@hotmail.com>
2023-05-10 22:10:50 -0300

    arranging folders

--- project/.storybook/preview.js
+++ project/.storybook/preview.js
@@ -1,6 +1,6 @@
 import { ThemeProvider } from 'styled-components';
-import { GlobalStyles } from '../src/components/styles/global-styles';
-import { theme } from '../src/components/styles/theme';
+import { GlobalStyles } from '../src/styles/global-styles';
+import { theme } from '../src/styles/theme';
 
 /** @type { import('@storybook/react').Preview } */
 const preview = {

5b0b3a79fdf55694e612bb0092d8c019c9556f6d
Shystra <jlsp.2019@hotmail.com>
2023-05-10 22:32:13 -0300

    creating folder base

---
+++ project/src/Base/mock.js
@@ -0,0 +1,12 @@
+import linksMock from '../components/NavLinks/mock';
+
+
+
+export default {
+    links: linksMock,
+    logoData: {
+        text:'logo',
+        link: '#'
+    },
+    footerHtml: '<p>Teste footer</p>'
+}
---
+++ project/src/Base/stories.jsx
@@ -0,0 +1,29 @@
+import { Base } from '.';
+import mock from './mock';
+import {GridText} from '../components/GridText';
+import gridMock from '../components/GridText/mock';
+
+
+
+export default {
+    title: 'Base',
+    component: Base,
+    args: {
+        children: (
+            <>
+                <GridText {...gridMock}/>
+            
+            </>
+        ),
+        ...mock,
+    },
+
+};
+
+export const Template = (args) => {
+    return (
+        <div>
+            <Base {...args} />
+        </div>
+    );
+};

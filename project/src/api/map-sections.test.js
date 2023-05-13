2bd423d8334930e52623bf82bfb944240256987e
Shystra <jlsp.2019@hotmail.com>
2023-05-13 14:59:29 -0300

    just map section

--- project/src/api/map-sections.test.js
+++ project/src/api/map-sections.test.js
@@ -1,67 +1,144 @@
-import { mapSections, mapSectionTwoColumns } from "./map-sections";
+import { mapSectionContent, mapSections, mapSectionTwoColumns } from "./map-sections";
 
 
 describe('map-sections', () => {
-    it('should render predefined section if no data', () => {
-      const data = mapSections();
-        expect(data).toEqual([]);
+  it('should render predefined section if no data', () => {
+    const data = mapSections();
+    expect(data).toEqual([]);
+  });
+
+  it('should map section two columns', () => {
+    const data = mapSectionTwoColumns();
+    expect(data.background).toBe(false);
+    expect(data.component).toBe('');
+    expect(data.sectionId).toBe('');
+    expect(data.srcImg).toBe('');
+    expect(data.text).toBe('');
+    expect(data.title).toBe('');
+  });
+
+  it('should map section two columns t', () => {
+    const data = mapSectionTwoColumns({
+      __component: 'section.section-two-columns',
+      _id: '602fdf2d540c00269e056178',
+      title: 'title',
+      description: 'abc',
+      metadata: {
+        background: true,
+        _id: '602fdf2d540c00269e05617b',
+        name: 'contact',
+        section_id: 'contact',
+        __v: 0,
+        id: '602fdf2d540c00269e05617b',
+      },
+      __v: 1,
+      image: {
+        _id: '602fdc2b540c00269e05616a',
+        name: 'javascript.svg',
+        alternativeText: 'Desenho de pessoas segurando logos do CSS, JS e HTML',
+        caption: 'Desenho de pessoas segurando logos do CSS, JS e HTML',
+        hash: 'javascript_b57bf48cda',
+        ext: '.svg',
+        mime: 'image/svg+xml',
+        size: 30.31,
+        url: 'a.svg',
+        provider_metadata: {
+          public_id: 'javascript_b57bf48cda',
+          resource_type: 'image',
+        },
+        provider: 'cloudinary',
+        width: 1030,
+        height: 730,
+        related: ['602fdf2d540c00269e056178', '602fdf2d540c00269e056172'],
+        createdAt: '2021-02-19T15:41:31.808Z',
+        updatedAt: '2021-02-19T15:54:22.590Z',
+        __v: 0,
+        id: '602fdc2b540c00269e05616a',
+      },
+      id: '602fdf2d540c00269e056178',
     });
-  
-    it('should map section two columns', () => {
-        const data = mapSectionTwoColumns();
-            expect(data.background).toBe(false);
-                expect(data.component).toBe('');
-                    expect(data.sectionId).toBe('');
-                expect(data.srcImg).toBe('');
-            expect(data.text).toBe('');
-        expect(data.title).toBe('');
-    });
-  
-    it('should map section two columns with components', () => {
-      const data = mapSectionTwoColumns({
-        __component: 'section.section-two-columns',
-        _id: '602fdf2d540c00269e056178',
-        title: 'title',
-        description: 'abc',
+    expect(data.background).toBe(true);
+    expect(data.component).toBe('section.section-two-columns');
+    expect(data.sectionId).toBe('contact');
+    expect(data.srcImg).toBe('a.svg');
+    expect(data.text).toBe('abc');
+    expect(data.title).toBe('title');
+  });
+
+  it('should map section content', () => {
+    const data = mapSectionContent();
+    expect(data.background).toBe(false);
+    expect(data.component).toBe('');
+    expect(data.sectionId).toBe('');
+    expect(data.title).toBe('');
+    expect(data.html).toBe('');
+
+    // {
+    //   "__component": "section.section-content",
+    //   "_id": "602fdf2d540c00269e056177",
+    //   "title": "Pricing",
+    //   "content": "<p>The release of Apple Silicon-based Macs at the end of last year generated a flurry of news coverage and some surprises at the machine’s performance. This post details some background information on the experience of porting Firefox to run natively on these CPUs.</p><p>We’ll start with some background on the Mac transition and give an overview of Firefox internals that needed to know about the new architecture, before moving on to the concept of Universal Binaries.</p><figure class=\"table\"><table><thead><tr><th>Title 1</th><th>Title 2</th><th>Title 3</th><th>Title 4</th><th>Title 5</th></tr></thead><tbody><tr><td>Content 1</td><td>Content 2</td><td>Content 3</td><td>Content 3</td><td>Content 3</td></tr><tr><td>Content 1</td><td>Content 2</td><td>Content 3</td><td>Content 3</td><td>Content 3</td></tr><tr><td>Content 1</td><td>Content 2</td><td>Content 3</td><td>Content 3</td><td>Content 3</td></tr><tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>Testando</td></tr></tbody></table></figure>",
+    //   "metadata": {
+    //     "background": false,
+    //     "_id": "602fdf2d540c00269e056179",
+    //     "name": "pricing",
+    //     "section_id": "pricing",
+    //     "__v": 0,
+    //     "id": "602fdf2d540c00269e056179"
+    //   },
+    //   "__v": 1,
+    //   "id": "602fdf2d540c00269e056177"
+    // },
+
+
+
+
+  });
+
+  it('should map section content with no data', () => {
+    const data = mapSectionContent(
+      {
+        __component: "section.section-content",
+        _id: "602fdf2d540c00269e056177",
+        title: "Pricing",
+        content: 'abc',
         metadata: {
-          background: true,
-          _id: '602fdf2d540c00269e05617b',
-          name: 'contact',
-          section_id: 'contact',
+          background: false,
+          _id: "602fdf2d540c00269e056179",
+          name: "pricing",
+          section_id: "pricing",
           __v: 0,
-          id: '602fdf2d540c00269e05617b',
+          id: "602fdf2d540c00269e056179"
         },
         __v: 1,
-        image: {
-          _id: '602fdc2b540c00269e05616a',
-          name: 'javascript.svg',
-          alternativeText: 'Desenho de pessoas segurando logos do CSS, JS e HTML',
-          caption: 'Desenho de pessoas segurando logos do CSS, JS e HTML',
-          hash: 'javascript_b57bf48cda',
-          ext: '.svg',
-          mime: 'image/svg+xml',
-          size: 30.31,
-          url: 'a.svg',
-          provider_metadata: {
-            public_id: 'javascript_b57bf48cda',
-            resource_type: 'image',
-          },
-          provider: 'cloudinary',
-          width: 1030,
-          height: 730,
-          related: ['602fdf2d540c00269e056178', '602fdf2d540c00269e056172'],
-          createdAt: '2021-02-19T15:41:31.808Z',
-          updatedAt: '2021-02-19T15:54:22.590Z',
-          __v: 0,
-          id: '602fdc2b540c00269e05616a',
-        },
-        id: '602fdf2d540c00269e056178',
-      });
-    expect(data.background).toBe(true);
-        expect(data.component).toBe('section.section-two-columns');
-            expect(data.sectionId).toBe('contact');
-                expect(data.srcImg).toBe('a.svg');
-            expect(data.text).toBe('abc');
-        expect(data.title).toBe('title');
-    });
-  });
+        id: "602fdf2d540c00269e056177"
+      },
+    );
+    expect(data.background).toBe(false);
+    expect(data.component).toBe('section.section-content');
+    expect(data.sectionId).toBe('pricing');
+    expect(data.title).toBe('Pricing');
+    expect(data.html).toBe('abc');
+
+    // {
+    //   "__component": "section.section-content",
+    //   "_id": "602fdf2d540c00269e056177",
+    //   "title": "Pricing",
+    //   "content": "<p>The release of Apple Silicon-based Macs at the end of last year generated a flurry of news coverage and some surprises at the machine’s performance. This post details some background information on the experience of porting Firefox to run natively on these CPUs.</p><p>We’ll start with some background on the Mac transition and give an overview of Firefox internals that needed to know about the new architecture, before moving on to the concept of Universal Binaries.</p><figure class=\"table\"><table><thead><tr><th>Title 1</th><th>Title 2</th><th>Title 3</th><th>Title 4</th><th>Title 5</th></tr></thead><tbody><tr><td>Content 1</td><td>Content 2</td><td>Content 3</td><td>Content 3</td><td>Content 3</td></tr><tr><td>Content 1</td><td>Content 2</td><td>Content 3</td><td>Content 3</td><td>Content 3</td></tr><tr><td>Content 1</td><td>Content 2</td><td>Content 3</td><td>Content 3</td><td>Content 3</td></tr><tr><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>Testando</td></tr></tbody></table></figure>",
+    //   "metadata": {
+    //     "background": false,
+    //     "_id": "602fdf2d540c00269e056179",
+    //     "name": "pricing",
+    //     "section_id": "pricing",
+    //     "__v": 0,
+    //     "id": "602fdf2d540c00269e056179"
+    //   },
+    //   "__v": 1,
+    //   "id": "602fdf2d540c00269e056177"
+    // },
+
+
+
+
+  })
+});

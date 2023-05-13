26e083273e04eda2f68424b8ab846817025ecc9e
Shystra <jlsp.2019@hotmail.com>
2023-05-13 11:58:28 -0300

    testing map-data

--- project/src/api/map-data.js
+++ project/src/api/map-data.js
@@ -1,3 +1,19 @@
-export const mapData = () => {
-    
-}
+export const mapData = (pagesData = [{}]) => {
+
+
+    return pagesData.map((data) => {
+        const {footerHtml = '', slug = '', title = '', sections = [], menu = {} } = data;
+
+        return {
+            footerHtml,
+            slug,
+            title,
+            sections,
+            menu,
+        };
+    })
+};
+
+const {inspect} = require('util');
+    const dados = require('./dados.json');
+        console.log(inspect(mapData(dados), null, null, true));

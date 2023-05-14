c5569115d73d4fb92683ecae7974383c1c30a61c
Shystra <jlsp.2019@hotmail.com>
2023-05-14 17:38:31 -0300

    just configuring

--- project/src/api/map-data.js
+++ project/src/api/map-data.js
@@ -3,24 +3,20 @@ import { mapSections } from './map-sections';
 
 export const mapData = (pagesData = [{}]) => {
     return pagesData.map((data) => {
-        const {
-            footer_text: footerHtml = '', 
-                slug = '', 
-                    title = '', 
-                        sections = [], 
-                            menu = {} 
-            } = data;
-
-        return {
-            footerHtml,
-                slug,
-                    title,
-                        sections: mapSections(sections),
-                            menu: mapMenu(menu),
-        };
-    })
-};
-
-const {inspect} = require('util');
-    const dados = require('./dados.json');
-        console.log(inspect(mapData(dados), null, null, true));
+      const {
+        footer_text: footerHtml = '',
+        slug = '',
+        title = '',
+        sections = [],
+        menu = {},
+      } = data;
+  
+      return {
+        footerHtml,
+        slug,
+        title,
+        sections: mapSections(sections),
+        menu: mapMenu(menu),
+      };
+    });
+  };

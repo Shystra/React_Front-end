6d04e2a5096469133f97d51c1ca63ab8900858fb
Shystra <jlsp.2019@hotmail.com>
2023-05-14 17:38:26 -0300

    just configuring

--- project/src/templates/Home/index.jsx
+++ project/src/templates/Home/index.jsx
@@ -1,10 +1,34 @@
 
 import { Base } from '../Base';
 import { mockBase } from '../Base/mock';
-import * as Styled from './styles';
-
+import { useEffect } from 'react';
+import {mapData} from '../../api/map-data'
+import { useState } from 'react';
 
 function Home() {
+  // const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const load = async () => {
+      const data = await fetch('http://localhost:1337/api/pages/?slug=landing-page',);
+        const json = await data.json();
+          const pageData = mapData(json);
+            console.log(pageData);
+      // setData(pageData[0]);
+
+      console.log(json);
+    };
+    load();
+  }, []);
+
+  // if (data === undefined){
+  //   return <h1>Pagina não encontrada</h1>
+  // }
+  // if (data && !data.slug){
+  //   return <h1>Carregando...</h1>
+  // }
+
+
   return <Base {...mockBase} />;
   
 };
